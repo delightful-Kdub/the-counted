66cd921fbaa46fe4a8cfde57009002c83627713f
delightful-Kdub <krysta@SchmutzigKillahKdub.fios-router.home>
2016-10-06 21:12:11 -0400

    Start building with the API connection

--- js/script.js
+++ js/script.js
@@ -3,11 +3,66 @@
 //http://open-platform.theguardian.com/documentation/
 
 
+var data = {}
 
-  $.ajax({
-  	url: "https://thecountedapi.com/api/counted",
-    success: function(res) {
-    	console.log(res);
-      $("counted").append("<li>" + name + "</li>");
-    }
-})
+var raceInput = document.getElementById('raceInput').value;
+var sexInput = document.getElementById('sexInput').value;
+var armsInput = document.getElementById('armsInput').value;
+var ageInput = document.getElementById('ageInput').value;
+
+
+//race?
+
+fetch('https://thecountedapi.com/api/counted/?armed=non-lethal firearm' + raceInput).then(function(response) {
+  var contentType = response.headers.get("content-type");
+  if(contentType && contentType.indexOf("application/json") !== -1) {
+    return response.json().then(function(json) {
+      raceOutput.innerHTML = json.length; // data= json;
+    });
+  } else {
+    console.log("Oops, we haven't got JSON!");
+  }
+});
+
+
+
+//gender?
+
+fetch('https://thecountedapi.com/api/counted/?sex=' + sexInput).then(function(response) {
+  var contentType = response.headers.get("content-type");
+  if(contentType && contentType.indexOf("application/json") !== -1) {
+    return response.json().then(function(json) {
+      sexOutput.innerHTML = json.length;
+    });
+  } else {
+    console.log("Oops, we haven't got JSON!");
+  }
+});
+
+
+//armed?
+
+fetch('https://thecountedapi.com/api/counted/?armed=' + armsInput).then(function(response) {
+  var contentType = response.headers.get("content-type");
+  if(contentType && contentType.indexOf("application/json") !== -1) {
+    return response.json().then(function(json) {
+      armsOutput.innerHTML = json.length;
+    });
+  } else {
+    console.log("Oops, we haven't got JSON!");
+  }
+});
+
+
+//age?
+
+fetch('https://thecountedapi.com/api/counted/?age=' + ageInput).then(function(response) {
+  var contentType = response.headers.get("content-type");
+  if(contentType && contentType.indexOf("application/json") !== -1) {
+    return response.json().then(function(json) {
+      ageOutput.innerHTML = json.length;
+    });
+  } else {
+    console.log("Oops, we haven't got JSON!");
+  }
+});
